e9551d1dd2ca808e004e037efd94d9b72e3d387a
Rafael Almendra <rafaelalmendra28@gmail.com>
2025-07-22 00:55:49 -0300

    feat: adicionar logo.webp e atualizar a homepage com novo conteúdo e estilos

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,10 +1,10 @@
-import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
-import { getPayload } from 'payload'
 import React from 'react'
-import { fileURLToPath } from 'url'
+import Image from 'next/image'
+import { headers as getHeaders } from 'next/headers.js'
 
+import { getPayload } from 'payload'
 import config from '@/payload.config'
+
 import './styles.css'
 
 export default async function HomePage() {
@@ -13,22 +13,30 @@ export default async function HomePage() {
   const payload = await getPayload({ config: payloadConfig })
   const { user } = await payload.auth({ headers })
 
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
   return (
     <div className="home">
       <div className="content">
         <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
+          <source srcSet="/logo.webp" />
+          <Image alt="Logo" src="/logo.webp" width={154} height={39} />
         </picture>
-        {!user && <h1>Bem-vindo ao Blog do Juridiq.</h1>}
-        {user && <h1>Bem-vindo(a) de volta, {user.email}</h1>}
+
+        {!user && (
+          <h1
+            style={{
+              fontSize: '1.75rem',
+              fontWeight: 'bold',
+              marginBottom: '1rem',
+            }}
+          >
+            Bem-vindo ao Blog do Juridiq.
+          </h1>
+        )}
+        {user && (
+          <h1 style={{ fontSize: '1.75rem', fontWeight: 'bold', marginBottom: '1rem' }}>
+            Bem-vindo(a) de volta, {user.email}
+          </h1>
+        )}
         <div className="links">
           <a
             className="admin"
@@ -36,7 +44,7 @@ export default async function HomePage() {
             rel="noopener noreferrer"
             target="_blank"
           >
-            Ir para o painel de administração
+            Acessar Admin
           </a>
         </div>
       </div>
